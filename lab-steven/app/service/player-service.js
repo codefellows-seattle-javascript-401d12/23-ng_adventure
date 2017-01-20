babfd2ca7fddcae170fd5e50fe4677352bbb801f
Steven Bateman <batemanvo@gmail.com>
2017-01-19 19:55:26 -0800

    Created basic player service.

---
+++ lab-steven/app/service/player-service.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const angular = require('angular');
+const adventureGame = angular.module('adventureGame');
+
+adventureGame.factory('playerService', ['$log', '$q', 'mapService', playerService]);
+
+function playerService($log, $q, mapService) {
+  $log.debug('playerService');
+
+  let service = {};
+
+  service.player = {
+    name: 'Sample name',
+    location: 'room1'
+  };
+
+  service.history = [
+    {
+      desc: 'Welcome to the game',
+      location: service.player.location
+    }
+  ];
+
+  service.movePlayer = function(direction) {
+    return new $q((resolve, reject) => {
+      let newLocation = mapService.mapData[service.player.location][direction];
+
+      if (!newLocation) {
+        service.history.unshift({
+          desc: 'You can\'t go that way',
+          location: service.player.location
+        });
+        return reject('You can\'t go that way.');
+      }
+
+      service.player.location = newLocation;
+
+      service.history.unshift({
+        desc: mapService.mapData[newLocation].desc,
+        location: service.player.location,
+      });
+
+      return resolve(service.player.location);
+    });
+  };
+
+  return service;
+}
