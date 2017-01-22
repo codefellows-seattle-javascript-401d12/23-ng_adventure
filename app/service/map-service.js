150fd25cc4b2f8119e794bd552440928feae561d
Peter Kim <peterkim23@gmail.com>
2017-01-22 13:22:03 -0800

    setup map service file

--- app/service/map-service.js
+++ app/service/map-service.js
@@ -5,7 +5,7 @@ const ngSoundSearch = angular.module('ngSoundSearch');
 
 ngSoundSearch.factory('mapService', ['$log', mapService]);
 
-function mapServce($log) {
+function mapService($log) {
   $log.debug('mapService');
 
   let service = {};
@@ -25,9 +25,51 @@ function mapServce($log) {
       up: 'coatCheck'
     },
     pathwayToNoise: {
-      desc: 'Keep moving!',
-      right: 'superSonicStage'
+      desc: 'Keep moving through the crowd!',
+      right: 'superSonicStage',
+      left: 'coatCheck'
     },
-    
-  }
+    superSonicStage: {
+      desc: 'You made it to the Super Sonic Stage. Looks like the openers are still playing. You still got time!',
+      up: 'beerGarden',
+      left: 'pathwayToNoise',
+      right: 'pathwyToRelief'
+    },
+    beerGarden: {
+      desc: 'Nice, you found the beer garden! Grab a quick refreshment and chug it cause you done have much time.',
+      down: 'superSonicStage'
+    },
+    pathwayToRelief: {
+      desc: 'Keep on moving, you are getting closer to the main stage. Looks like a restroom is nearby if you need some relief.',
+      right: 'restrooms',
+      left: 'superSonicStage',
+      down: 'pathwayOfPulsation'
+    },
+    restrooms: {
+      desc: 'Take quick moment to use the restroom. You do not want to leave the main stage once you get in.',
+      left: 'pathwayToRelief'
+    },
+    pathwayOfPulsation: {
+      desc: 'The ground is starting to shake and your skin feels the rhythm.',
+      down: 'bassToTheFace'
+    },
+    bassToTheFace: {
+      desc: 'You are at the Bass to the Face stage. Some hard hitting beats is making your face melt, keep moving!',
+      up: 'pathwayOfPulsation',
+      left: 'pathwayToDrop'
+    },
+    pahtwayToDrop: {
+      desc: 'Just cut through this crowd and you are there!',
+      down: 'dynamicsOfLife',
+      left: 'nextBeerGarden'
+    },
+    nextBeerGarden: {
+      desc: 'Drink up quick! Your favoirte artist is about go on!',
+      right: 'pathwayToDrop',
+    },
+    dynamicsOfLife: {
+      desc: 'Yay! You made it the Dynamics of Life Main Stage! Your favorite artist is just about to start, you did it!'
+    }
+  };
+  return service;
 }
