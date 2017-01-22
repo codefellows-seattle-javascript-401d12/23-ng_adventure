e18d163e7c71a41acd17bc4cf8edaff3bccde11e
Steven Bateman <batemanvo@gmail.com>
2017-01-21 20:41:40 -0800

    Added a few room descriptions and rat description.

--- lab-steven/app/service/map-service.js
+++ lab-steven/app/service/map-service.js
@@ -18,37 +18,36 @@ function mapService($log, itemService, mobService) {
       items: [],
       mobs: []
     },
-    room1: {
-      title: 'Room 1',
-      desc: 'This is the description for room 1. It\'s just placeholder text and will thus be gone in the near future.',
+    gates: {
+      title: 'Gates of Tempest',
+      desc: 'Large wooden doors create the maw that leads into the city of Tempest. They\'ve been battered and worn down by the constant storming rain, one seeming as if it\'s ready to surrender to the elements and fall from its hinges. A cobblestone street leads south into the city, occasional breaks in the stone leading to muddy puddles of drowning earth. In the distance, the silhouette of an imposing tower looms over the cityscape, obscured by the sheet of rain.',
       exits: {
-        east: {
-          connection: 'room2',
-          locked: true,
-          unlockKey: itemService.gameItems.key
-        }
-      },
-      items: [itemService.gameItems.key, itemService.gameItems.key, itemService.gameItems.blueKey, itemService.gameItems.healingPotion, itemService.gameItems.healingPotion, itemService.gameItems.manaPotion],
-      mobs: [mobService.rat, mobService.turtle]
-    },
-    room2: {
-      title: 'Room 2',
-      desc: 'This will also be replaced at some point soon. Probably by something significantly more fascinating.',
-      exits: {
-        west: {
-          connection: 'room1',
-          locked: true
-        },
-        east: {
-          connection: 'room3',
+        south: {
+          connection: 'intersection',
           locked: false
         }
       },
-      items: []
+      items: [],
+      mobs: []
     },
-    room3: {
-      title: 'Room 3',
-      desc: 'This may come as a shock to you, but this is also just some placeholder text. Nothing to see here, folks. Move along.',
+    intersection: {
+      title: 'Crossroads of Main Street and Drusalya',
+      desc: 'Main street runs from the gates of Tempest to the North into the heart of the dreary city to the South. It\'s flanked on either side by a number of buildings that once were businesses or housing complexes for a litany of citizens. Now they\'re crumbling, dilapidated ghosts of their former glory. To the East is Drusalya Street, but the road has been washed out. Attempts at bridging the gap of water seem to have been long-forgotten.',
+      exits: {
+        north: {
+          connection: 'gates',
+          locked: false
+        },
+        south: {
+          connection: 'mainStreet',
+          locked: false
+        }
+      },
+      items: [mobService.rat]
+    },
+    mainStreet: {
+      title: 'Main Street',
+      desc: '',
       exits: {
         west: {
           connection: 'room2',
--- lab-steven/app/service/mob-service.js
+++ lab-steven/app/service/mob-service.js
@@ -13,12 +13,12 @@ function mobService($log, itemService) {
   service.rat = {
     keywords: ['rat', 'black', 'black rat'],
     shortDesc: 'a black rat',
-    longDesc: 'A large black rat is here, sniffing around curiously.',
-    description: 'It\'s a hairy black rat.',
+    longDesc: 'A large black rat is here, soaking wet from the rain, but sniffing around curiously.',
+    description: 'Looking every bit the part of a drowned rat that should\'ve escaped its sinking ship, this creature instead seems quite resilient to the environment. It appears quite well-fed, with a fullness to its admittedly large body. Its beady eyes frequently shift from staring blankly, taking in its surroudings, to focusing intently on some random unseen location or object. Occasionally it stops to futilely groom itself, licking its paws before running them over the back of its wet head.',
     attack: 'A large black rat bites you!',
     hp: 5,
     atk: 2,
-    inventory: [itemService.gameItems.key]
+    inventory: []
   };
 
   service.turtle = {
