51850ecd2b0b5f1d630bc5bd6c03d447eedb0bd1
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-23 03:35:14 -0800

    require game-history component

--- app/entry.js
+++ app/entry.js
@@ -11,3 +11,4 @@ require('./service/player-service.js');
 
 require('./component/gameboard/gameboard.js');
 require('./component/player-dashboard/player-dashboard.js');
+require('./component/game-history/game-history.js');
