e1f799e27703477419f6d50d1942191701572974
Megan Reardon <meganr@gmail.com>
2017-01-22 19:02:37 -0800

    ending working

--- app/service/map-service.js
+++ app/service/map-service.js
@@ -38,13 +38,23 @@ function mapService($log) {
       west: 'den'
     },
     den: {
-      description: 'You have stepped into what was once a comfortable den. Cold air strems into the room from an empty fireplace. The shelves of the empty bookcase sag from rot. There is a box of matches here. There is a door to the EAST that will take you back to the corridor.',
+      description: 'You have stepped into what was once a comfortable den. Cold air strems into the room from an empty fireplace. The shelves of the empty bookcase sag from rot. There is a door to the EAST that will take you back to the corridor.',
       east: 'corridor'
     },
+    // NOTE: saved below to use when have inventory working
+    // den: {
+    //   description: 'You have stepped into what was once a comfortable den. Cold air strems into the room from an empty fireplace. The shelves of the empty bookcase sag from rot. There is a box of matches here. There is a door to the EAST that will take you back to the corridor.',
+    //   east: 'corridor'
+    // },
     diningroom: {
-      description: 'You are in a dining room, light from outside comes through the cracked windows. Thick dust covers a huge table, no chairs remain. There is a fresh hand print in the dust. There is a candle here. A door to the WEST will take you back into the corridor.',
+      description: 'You are in a dining room, light from outside comes through the cracked windows. Thick dust covers a huge table, no chairs remain. There is a fresh hand print in the dust. A door to the WEST will take you back into the corridor.',
       west: 'corridor'
     },
+    // NOTE: saved below to use when have inventory working
+    // diningroom: {
+    //   description: 'You are in a dining room, light from outside comes through the cracked windows. Thick dust covers a huge table, no chairs remain. There is a fresh hand print in the dust. There is a candle here. A door to the WEST will take you back into the corridor.',
+    //   west: 'corridor'
+    // },
     kitchen: {
       description: 'You find yourself in a kitchen. A faucet is dripping into a large, rust stained sink. Peeling cabinet doors hang crooked on broken hinges. There are doors to the EAST and WEST. The door back to the corridor is to the SOUTH.',
       east: 'pantry',
@@ -52,17 +62,21 @@ function mapService($log) {
       west: 'cellar'
     },
     pantry: {
-      description: 'You find yourself in large pantry, dusty shelves hold a few pieces of broken crockery. There is a key here. The door back to the kitchen is to the WEST.',
+      description: 'You find yourself in large pantry, dusty shelves hold a few pieces of broken crockery. The door back to the kitchen is to the WEST.',
       west: 'kitchen'
     },
+    // NOTE: saved below to use when have inventory working
+    // pantry: {
+    //   description: 'You find yourself in large pantry, dusty shelves hold a few pieces of broken crockery. There is a key here. The door back to the kitchen is to the WEST.',
+    //   west: 'kitchen'
+    // },
     cellar: {
       description: 'You are in a small room, to the WEST is a door. It is locked.',
       east: 'kitchen',
       west: 'stairs'
     },
     stairs: {
-      description: 'A staircase leads down into pitch black.',
-      east: 'cellar'
+      description: 'A staircase leads down into pitch black. Despite feeling afraid you descend. You have been eaten by a grue. THE END.'
     }
   };
 
