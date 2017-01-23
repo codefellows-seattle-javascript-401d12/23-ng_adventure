cf9b6edfa3f5518b3e71e7efa4791bd7e4b0da51
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-23 01:13:28 -0800

    change location names to underscore conv for mult words

--- app/service/map-service.js
+++ app/service/map-service.js
@@ -15,75 +15,76 @@ function mapService($log) {
     start: {
       title: 'start of the game',
       description: 'Deep in space aboard the research ship Vasa. The \'west\' section of the ship has been evacuated while you go in to investiage a malfunction. Deep into the eerily quiet \'west\' section, you feel the craft shudder, and seconds later, the emergency system alerts you that the section has lost its main oxygen supply in an explosion. You grab an emergency oxygen tank and mask and prepare to head for the airlock that will allow you to escape to one of the other undamaged sections. Each move you make depletes your oxygen a little, so you\'ll have to conserve. You know that your tank won\'t give you enough oxygen to make it all the way, so you\'ll need to find another tank somewhere between here and the airlock...',
-      north: 'hallway1',
+      north: 'first_hallway',
     },
-    hallway1: {
+    first_hallway: {
       title: 'hallway',
       description: 'a long, dimly lit hallway with narrow door to the west',
-      north: 'junction1',
-      west: 'storageRoom',
+      north: 'first_junction',
+      south: 'start',
+      west: 'storage_room',
     },
-    storageRoom: {
+    storage_room: {
       title: 'storage room',
       description: 'a storage room, empty except for some robot spare parts. it\'s a dead end.',
-      east: 'hallway1',
+      east: 'first_hallway',
     },
-    junction1: {
+    first_junction: {
       title: 'junction',
       description: 'you\'ve reached the end of the dim hallway and you\'re at a junction. which way should you go?',
-      east: 'crewQuarters',
+      east: 'crew_quarters',
       north: 'kitchen',
-      south: 'hallway1',
+      south: 'first_hallway',
     },
     kitchen: {
       title: 'kitchen',
       description: 'a pot of split pea soup still sits warm on the stove.',
-      south: 'hallway1',
+      south: 'first_junction',
     },
-    crewQuarters: {
+    crew_quarters: {
       title: 'crew quarters',
       description: 'smelly socks still lie next to unmade bunks. this room has many possible exits... which one to choose?',
       east: 'bathroom',
       north: 'staircase',
-      south: 'exerciseRoom',
-      west: 'junction1',
+      south: 'exercise_room',
+      west: 'first_junction',
     },
     bathroom: {
       title: 'bathroom',
       description: 'unless you need to go, you\'d better turn around.',
-      west: 'crewQuarters',
+      west: 'crew_quarters',
     },
-    exerciseRoom: {
+    exercise_room: {
       title: 'exercise room',
       description: 'save the workout for when you have more oxygen.',
-      north: 'crewQuarters',
+      north: 'crew_quarters',
     },
     staircase: {
       title: 'staircase',
       description: 'up the stairs, toward the fresh air.',
-      east: 'junction2',
-      south: 'crewQuarters',
+      east: 'second_junction',
+      south: 'crew_quarters',
     },
-    junction2: {
+    second_junction: {
       title: 'junction',
       description: 'at the top of the stairs, another junction.',
-      east: 'recRoom',
+      east: 'rec_room',
       south: 'lab',
       west: 'staircase',
     },
-    recRoom: {
+    rec_room: {
       title: 'rec room',
       description: 'a little r&r would be nice right now, but there\'s no time for games.',
-      west: 'junction2',
+      west: 'second_junction',
     },
     lab: {
       title: 'lab',
       description: 'a creepy place to be alone in.',
       east: 'library',
-      north: 'junction2',
-      west: 'medicalStation',
+      north: 'second_junction',
+      west: 'medical_station',
     },
-    medicalStation: {
+    medical_station: {
       title: 'medical station',
       description: 'you found another oxygen tank!',
       east: 'lab',
@@ -91,21 +92,21 @@ function mapService($log) {
     library: {
       title: 'library',
       description: 'don\'t stop to read, you still have to get out.',
-      north: 'hallway2',
+      north: 'second_hallway',
       west: 'lab',
     },
-    hallway2: {
+    second_hallway: {
       title: 'hallway',
       description: 'another long, narrow hallway, with mysterious doors leading to who-knows-where.',
       east: 'alien',
-      north: 'engineRoom',
+      north: 'engine_room',
       south: 'library',
       west: 'airlock',
     },
-    engineRoom: {
+    engine_room: {
       title: 'engine room',
       description: 'a noisy, not to mention radioactive place.',
-      south: 'hallway2',
+      south: 'second_hallway',
     },
     alien: {
       title: 'room containing an alien',
