518827a3a9e8bf64338b0e75c2492d41eb8e6daf
Megan Reardon <meganr@gmail.com>
2017-01-22 19:49:27 -0800

    edits to gamepad

--- app/component/game-pad/game-pad.js
+++ app/component/game-pad/game-pad.js
@@ -26,11 +26,4 @@ function GamePadController($log, playerService) {
       $log.error(err);
     });
   };
-
-  // this.theEnd = function() {
-  //   if (this.player.location === this.mapService.mapData.stairs) {
-  //     console.log('THE END');
-  //   }
-  // };
-
 }
