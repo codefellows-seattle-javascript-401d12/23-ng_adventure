42686d924d52f884659b192c87fd0c28bc7d9f0c
Megan Reardon <meganr@gmail.com>
2017-01-25 11:45:10 -0800

    good example code in test file

--- app/test/ng-adventure-test.js
+++ app/test/ng-adventure-test.js
@@ -4,6 +4,75 @@ require('./lib/test-setup.js');
 
 const angular = require('angular');
 
+// controller defaults:
+// player start location - porch
+// player history current message - welcome, porch
+// player current pointed direction - north
+// player inventory - empty string
+
+// controller methods:
+// move player from porch, expect entryway
+// move player south from porch, expect 'no room that way' message
+// +1 in player history length after move player
+// expect inventory to be 'key' after enter pantry
+
+
+
+// example from docs
+describe('component: heroDetail', function() {
+  var $componentController;
+
+  beforeEach(module('heroApp'));
+  beforeEach(inject(function(_$componentController_) {
+    $componentController = _$componentController_;
+  }));
+
+  it('should expose a `hero` object', function() {
+    // Here we are passing actual bindings to the component
+    var bindings = {hero: {name: 'Wolverine'}};
+    var ctrl = $componentController('heroDetail', null, bindings);
+
+    expect(ctrl.hero).toBeDefined();
+    expect(ctrl.hero.name).toBe('Wolverine');
+  });
+
+  it('should call the `onDelete` binding, when deleting the hero', function() {
+    var onDeleteSpy = jasmine.createSpy('onDelete');
+    var bindings = {hero: {}, onDelete: onDeleteSpy};
+    var ctrl = $componentController('heroDetail', null, bindings);
+
+    ctrl.delete();
+    expect(onDeleteSpy).toHaveBeenCalledWith({hero: ctrl.hero});
+  });
+
+  it('should call the `onUpdate` binding, when updating a property', function() {
+    var onUpdateSpy = jasmine.createSpy('onUpdate');
+    var bindings = {hero: {}, onUpdate: onUpdateSpy};
+    var ctrl = $componentController('heroDetail', null, bindings);
+
+    ctrl.update('foo', 'bar');
+    expect(onUpdateSpy).toHaveBeenCalledWith({
+      hero: ctrl.hero,
+      prop: 'foo',
+      value: 'bar'
+    });
+  });
+
+});
+
+// example from stack overflow
+// var $componentController;
+// beforeEach(inject(function($rootScope, _$componentController_){
+//     scope = $rootScope.$new();
+//     $componentController = _$componentController_;
+//     controller = $componentController('mainViewer',
+//                                      {$scope:scope},
+//                                      {name: 'Main Component!'} );
+// }));
+
+
+// all below are tests for previous lab
+// kept here for example
 describe('Cowsay Controller', function() {
 
   beforeEach(() => {
