0ad0870fbd9baee64d47d5e4465d9c88a292527a
Megan Reardon <meganr@gmail.com>
2017-01-25 13:56:26 -0800

    have initial controller setting tests passing

--- app/test/ng-adventure-test.js
+++ app/test/ng-adventure-test.js
@@ -4,12 +4,7 @@ require('./lib/test-setup.js');
 
 const angular = require('angular');
 
-// controller defaults:
-// player start location - porch
-// player history current message - welcome, porch
-// player current pointed direction - north
-// player inventory - empty string
-
+// NOTE: add the following
 // controller methods:
 // move player from porch, expect entryway
 // move player south from porch, expect 'no room that way' message
@@ -17,7 +12,7 @@ const angular = require('angular');
 // expect inventory to be 'key' after enter pantry
 
 
-describe('component: playerInfo', function() {
+describe('Controller Initial States', function() {
   var $componentController;
 
   beforeEach(angular.mock.module('ngAdventure'));
@@ -26,138 +21,27 @@ describe('component: playerInfo', function() {
   }));
 
   it('should show the player location at the porch', function () {
-    // var bindings = {player: {location: 'porch'}};
     var bindings = {location: 'porch'};
-    // var bindings = {player: {location: 'porch'}};
     var ctrl = $componentController('playerInfo', null, bindings);
-    console.log('\n\nCTRL is:', ctrl, '\n\n');
-    // expect(ctrl.location).toBeDefined();
     expect(ctrl.location).toBe('porch');
   });
 
   it('should return an empty inventory', function () {
     var bindings = {inventory: ''};
     var ctrl = $componentController('playerInfo', null, bindings);
-    console.log('\n\nCTRL is:', ctrl, '\n\n');
     expect(ctrl.inventory).toBe('');
   });
 
   it('should have the selected location as north', function() {
     var bindings = {directions: 'north'};
     var ctrl = $componentController('gamePad', null, bindings);
-    console.log('\n\nCTRL is:', ctrl, '\n\n');
     expect(ctrl.directions).toBe('north');
   });
 
+  it('should have a game history length of one', function() {
+    var bindings = '';
+    var ctrl = $componentController('gameHistory', null, bindings);
+    expect(ctrl.history.length).toEqual(1);
+  });
+
 });
-
-
-
-
-// example from docs
-// describe('component: heroDetail', function() {
-//   var $componentController;
-//
-//   beforeEach(module('heroApp'));
-//   beforeEach(inject(function(_$componentController_) {
-//     $componentController = _$componentController_;
-//   }));
-//
-//   it('should expose a `hero` object', function() {
-//     // Here we are passing actual bindings to the component
-//     var bindings = {hero: {name: 'Wolverine'}};
-//     var ctrl = $componentController('heroDetail', null, bindings);
-//
-//     expect(ctrl.hero).toBeDefined();
-//     expect(ctrl.hero.name).toBe('Wolverine');
-//   });
-//
-//   it('should call the `onDelete` binding, when deleting the hero', function() {
-//     var onDeleteSpy = jasmine.createSpy('onDelete');
-//     var bindings = {hero: {}, onDelete: onDeleteSpy};
-//     var ctrl = $componentController('heroDetail', null, bindings);
-//
-//     ctrl.delete();
-//     expect(onDeleteSpy).toHaveBeenCalledWith({hero: ctrl.hero});
-//   });
-//
-//   it('should call the `onUpdate` binding, when updating a property', function() {
-//     var onUpdateSpy = jasmine.createSpy('onUpdate');
-//     var bindings = {hero: {}, onUpdate: onUpdateSpy};
-//     var ctrl = $componentController('heroDetail', null, bindings);
-//
-//     ctrl.update('foo', 'bar');
-//     expect(onUpdateSpy).toHaveBeenCalledWith({
-//       hero: ctrl.hero,
-//       prop: 'foo',
-//       value: 'bar'
-//     });
-//   });
-//
-// });
-
-// example from stack overflow
-// var $componentController;
-// beforeEach(inject(function($rootScope, _$componentController_){
-//     scope = $rootScope.$new();
-//     $componentController = _$componentController_;
-//     controller = $componentController('mainViewer',
-//                                      {$scope:scope},
-//                                      {name: 'Main Component!'} );
-// }));
-
-
-// all below are tests for previous lab
-// // kept here for example
-// describe('Cowsay Controller', function() {
-//
-//   beforeEach(() => {
-//     angular.mock.module('cowsayApp');
-//     angular.mock.inject($controller => {
-//       this.cowsayAppCtrl = new $controller('CowsayAppController');
-//     });
-//   });
-//
-//   describe('initial properties', () => {
-//     it('title property should equal Welcome to Cowville!', () => {
-//       expect(this.cowsayAppCtrl.title).toBe('Welcome to Cowville!');
-//     });
-//   });
-//
-//   it('history property should be an empty array', () => {
-//     expect(Array.isArray(this.cowsayAppCtrl.history)).toBe(true);
-//   });
-//
-//   it('list of cowfiles should show proper cowfiles', () => {
-//     cowsay.list((err, list) => {
-//       expect(this.cowsayAppCtrl.cowfiles).toEqual(list);
-//       expect(this.cowsayAppCtrl.current).toEqual(list[0]);
-//     });
-//   });
-//
-//   describe('#update', () => {
-//     it('should return a cow that says testing', () => {
-//       let expected = cowsay.say({ text: 'testing', f: this.cowsayAppCtrl.current });
-//       let result = this.cowsayAppCtrl.update('testing');
-//       expect(result).toEqual(expected);
-//     });
-//   });
-//
-//   describe('#speak', () => {
-//     it('should return a cow that says testing', () => {
-//       let expected = cowsay.say({ text: 'testing', f: this.cowsayAppCtrl.current });
-//       this.cowsayAppCtrl.speak('testing');
-//       expect(this.cowsayAppCtrl.spoken).toEqual(expected);
-//       expect(this.cowsayAppCtrl.history[0]).toEqual(expected);
-//     });
-//   });
-//
-//   describe('#undo', () => {
-//     it('should undo a cow', () => {
-//       this.cowsayAppCtrl.speak('testing the undo');
-//       this.cowsayAppCtrl.undo();
-//       expect(this.cowsayAppCtrl.history.length).toEqual(0);
-//     });
-//   });
-//
-// });
